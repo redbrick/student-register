903c74d146840bf34e491383b185c8f60e97d452
benmcmahon100 <benmcmahon100@gmail.com>
2017-10-12 23:17:42 +0100

    Fix script

--- importer.js
+++ importer.js
@@ -6,6 +6,7 @@ const fs = require('fs-extra');
 fs.readFile('./users.csv', 'utf-8').then(async data => {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
+  data = data.split(/\,/gi);
   await page.goto('https://loop.dcu.ie', {waitUntil: 'networkidle'});
   // Type our query into the search bar
 
@@ -20,18 +21,22 @@ fs.readFile('./users.csv', 'utf-8').then(async data => {
     await page.type(process.env.pw);
     await page.click('button[type="submit"]');
 
-    setTimeout(async cans => {
-      data.split(/\r|\n/gi).forEach(async studentID => {
-        await page.goto('https://google.com', {waitUntil: 'networkidle'});
-        await page.goto('https://websvc.dcu.ie/clubs/socs/register', {waitUntil: 'networkidle'});
-        await page.waitForSelector('#form_id');
-        await page.click('#form_id');
-        await page.type(studentID);
-        await page.click('button[type="submit"]');
-        await page.waitForSelector('input[type="checkbox"]');
-        await page.click('input[type="checkbox"]');
-        await page.click('button[type="submit"]');
+    setTimeout(() => {
+      page.goto('https://google.com', {waitUntil: 'networkidle'}).then(async nothing => {
+        while (data.length > 0) {
+          let studentID = await data.shift();
+          await page.goto('https://websvc.dcu.ie/clubs/socs/register', {waitUntil: 'networkidle'})
+            .then(async nothing => {
+              await page.waitForSelector('#form_id');
+              await page.click('#form_id');
+              await page.type(studentID);
+              await page.click('button[type="submit"]');
+              await page.waitForSelector('input[type="checkbox"]');
+              await page.click('input[type="checkbox"]');
+              await page.click('button[type="submit"]');
+            });
+        }
       });
-    }, 6000);
+    }, 3000);
   }, 1000);
 });
